3fe77f99ba22a1a628147d2f4f683fd7bf4264c8
Mohammad Amin <mohammadaminmiri22@gmail.com>
2022-07-01 19:17:02 +0430

    Added project's functionality

--- main.py
+++ main.py
@@ -9,5 +9,29 @@ from motor.motor_asyncio import AsyncIOMotorClient
 app = FastAPI()
 
 
+# Initializing Database
+client = AsyncIOMotorClient("mongodb://mongodb:27017/")
+db = client.get_database("my_database")
+collection = db.get_collection("csv_collection")
+
+
+async def insert_csv(uploaded_file: UploadFile):
+    df = pd.read_csv(uploaded_file.file)
+    await collection.insert_many(df.to_dict(orient="records"))
+
+
+@app.post("/csv/")
+async def read_csv(file: UploadFile):
+    if file.content_type == "text/csv":
+        await insert_csv(file)
+        resp_body = jsonable_encoder({"detail": "csv was added to database successfully"})
+        return JSONResponse(status_code=202, content=resp_body)
+    raise HTTPException(status_code=400, detail="Only CSV")
+
+
+@app.get("/")
+async def get_csv_collection():
+    return list(collection.find({}, {"_id": False}))
+
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
